015116ada5be0bfe0a85b68d65c589f94b8ce119
megumitakahira <51527680+megumitakahira@users.noreply.github.com>
2025-07-01 18:29:31 +0900

    feat: implement health check endpoint
    Github-Issue: #2

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -7,8 +7,7 @@ from api.main import app
 @pytest.mark.anyio
 async def test_health_check() -> None:
     """Healtcheck endpoint returns 200."""
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:  # noqa: F841
-        # response = await client.get("/health")
-        # assert response.status_code == 200
-        # assert response.json() == {"status": "ok"}
-        pass
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        response = await client.get("/health")
+        assert response.status_code == 200
+        assert response.json() == {"status": "ok"}
